10ab3b3869118a48cafffb2770ff25276f3d342a
Lookscope Team <48394527+lookscope@users.noreply.github.com>
2021-08-08 15:30:21 -0400

    Add files via upload

---
+++ precache-manifest.3bcd590d8d2a8da628ee6555087f3168.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c49dee55faf7e519fc790fac2e5f2ee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9957e5b6e0c0058b9f67",
+    "url": "./static/css/main.3192a0c3.chunk.css"
+  },
+  {
+    "revision": "3d721862d242e815cdd7",
+    "url": "./static/js/2.1c6ae1d1.chunk.js"
+  },
+  {
+    "revision": "23900771a863482f56e68d68cf1f3072",
+    "url": "./static/js/2.1c6ae1d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9957e5b6e0c0058b9f67",
+    "url": "./static/js/main.e19d032f.chunk.js"
+  },
+  {
+    "revision": "78f91ece360930b4b5cf",
+    "url": "./static/js/runtime-main.e67b24ca.js"
+  },
+  {
+    "revision": "345373ad54a93ffd5b25139d379e2690",
+    "url": "./static/media/Lookscope-hero-photo-w4100px.345373ad.jpg"
+  },
+  {
+    "revision": "d197312b1b022a58283ee0ec55b7a04f",
+    "url": "./static/media/Lookscope-hero-photo.d197312b.jpg"
+  },
+  {
+    "revision": "c1848e19e236976d39f5df5a03c70256",
+    "url": "./static/media/back-button.c1848e19.svg"
+  }
+]);
